69a35264767509a89ef14319c27d40516c573779
Yashamal-Melaku <yashamalmelaku@gmail.com>
2025-05-13 04:39:56 +0300

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import './components/search.jsx';
 import './App.css';
-const App = () =>{
-  return (
-  <main>
-    <div className="pattern"/>
+import Search from './components/search.jsx';
+import { useState } from 'react';
 
-    <div className='wrapper'>
-      <header className="App-header">
-        <img src="./hero.png" alt='Hero Banner'/> 
-        <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
-      
-      </header>
-      <p>Search</p>
-    </div>
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('I am Batman');
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header className="App-header">
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </main>
   );
 }
