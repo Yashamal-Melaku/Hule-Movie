2bd5b1b45d73616c704029004a431a1ba995d616
Yashamal-Melaku <yashamalmelaku@gmail.com>
2025-05-21 14:40:03 +0300

    HULU Movie

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="text-white text-3xl">
-      {searchTerm}
+    <div className="search">
+    <div>
+          <img src="search.svg" alt="search" />
+          <input type="text" placeholder="Serch throu thousend of movies"
+          value={searchTerm}
+          onChange={(Event)=> setSearchTerm(Event.target.value)} />
+    </div>
     </div>
   );
 };
